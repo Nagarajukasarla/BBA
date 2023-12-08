4de790b2f6bdcfb86064e29dc533f2b0dbff4530
nagaraju <instanagrocks@gmail.com>
2023-12-08 16:25:45 +0530

    defined invoiceItem

--- backend/src/main/java/com/bba/Backend/requestModels/InvoiceRequest.java
+++ backend/src/main/java/com/bba/Backend/requestModels/InvoiceRequest.java
@@ -1,5 +1,8 @@
 package com.bba.Backend.requestModels;
 
 public class InvoiceRequest {
-
+    public Integer customerNumber;
+    public String paymentMode;
+    public List<InvoiceItem> items;
+    public Double discount;
 }
